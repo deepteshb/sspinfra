0cf0f4731ac6dfbf1191171b3fcf7f6a4954c6cd
Deeptesh Bhattacharya <deeptesh_b@persistent.com>
2022-01-22 22:01:49 +0530

    updated get_response func in database.py

--- database.py
+++ database.py
@@ -7,9 +7,6 @@ conn = sqlite3.connect('sspinfra.db', check_same_thread=False)
 
 def get_response():
     cur = conn.cursor()
-    query = cur.execute('select * from platform')
+    query = cur.execute('select * from platform').fetchall()
     return print(query)
-    
-
-    
-     
+    conn.close()
