5ce3fd80f981df3b6eca72c32519bba4b5def7e7
Deeptesh Bhattacharya <deeptesh_b@persistent.com>
2022-02-05 00:07:42 +0530

    implemented the functionality to pull data for customers from database and add to dropdown

--- app.py
+++ app.py
@@ -1,18 +1,117 @@
-from flask import Flask,render_template,url_for,session,g,request,session,redirect
+#============================================================================
+#ALL IMPORTS
+from flask import Flask,render_template,url_for,jsonify,session,g,request,session,redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import SelectField, StringField
+from wtforms_sqlalchemy.fields import QuerySelectField
+from flask_cors import CORS
 import sqlite3
 import os
 import datetime
 import sys
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = os.urandom(24)
+#============================================================================
+#CORE APP INITIALIZATION
 
-#database helpers
-@app.teardown_appcontext
-def close_db(error):
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
+
+app = Flask(__name__)
+
+#============================================================================
+#GLOBAL APP CONFIGURATION
+
+app.config['SECRET_KEY'] = os.urandom(24)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///D:/myProjects/sspinfra/sspinfra.db'
+
+#============================================================================
+#GLOBALDEFINITIONS
+
+
+
+
+#============================================================================
+#DBMODELS AND SCHEMA DEFINITIONS
+db = SQLAlchemy(app)
+
+
+#========TABLE GROUPS============
+class Groups(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    group = db.Column(db.String)
+    users = db.relationship('Users', backref='group')
+
+#========TABLE USERS============
+class Users(db.Model):
+    id = db.Column(db.Integer, nullable=False, primary_key=True)
+    username = db.Column(db.String)
+    authenticated = db.Column(db.Integer)
+    status = db.Column(db.Integer)
+    group_id = db.Column(db.Integer, db.ForeignKey('groups.id')) 
+
+#========TABLE CUSTOMER============
+class Customers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cname = db.Column(db.String())
+    versions = db.relationship('Versions', backref='cname')
+
+    def __repr__(self):
+        return (self.name)
+#========TABLE PRODUCT============
+class Products(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    pname = db.Column(db.String())
+    versions = db.relationship('Versions', backref='pname')
+
+#========TABLE COMPONENT============
+class Components(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    compname = db.Column(db.String())
+    versions = db.relationship('Versions', backref='compname')
+
+#========TABLE VERSION============
+class Versions(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    version = db.Column(db.String)
+    cust_id = db.Column(db.Integer, db.ForeignKey('customers.id')) 
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id')) 
+    comp_id = db.Column(db.Integer, db.ForeignKey('components.id')) 
+
+#============================================================================
+#DB-QUERIES
+def groups_query():
+    return Groups.query
+
+def users_query():
+    return Users.query
+
+
+def customer_query():
+    return Customers.query
+
+def products_query():
+    return Products.query
+
+def components_query():
+    return Components.query
+
+def version_query():
+    return Versions.query
+
+#============================================================================
+
+#FORMMODELS
+
+#========FORM-SCHEMA-LAUNCHINSTANCES============
+class LaunchInstanceForm(FlaskForm):
+    customer = SelectField('customer', choices=[])
+    product = SelectField('product', choices=[])
+    component = SelectField('component', choices=[])
+    version = SelectField('version', choices=[])
+    instance = StringField('instance')
+
+
+#==============================================================================
+#Application Routes
 
 @app.route("/", methods=['GET'])
 def index():
@@ -20,33 +119,20 @@ def index():
 
 @app.route("/login", methods=['POST', 'GET'])
 def login():
-        try:
-            if request.method == 'POST':
-                db = get_db()
-                name = request.form['userid']
-                uname = db.execute('SELECT username FROM users WHERE username=?', [name]).fetchone()
-                gid =  db.execute('SELECT gid FROM users WHERE username=?', [name]).fetchone()
-                if uname == name and gid == 1:
-                        return render_template('cisgservices.html')
-                elif uname == name and gid == 2:
-                            return render_template('userservicelist.html')
-                else:
-                    return render_template('loginerror.html')
-            else:
-                return render_template('login.html')
-        except TypeError:
-            return render_template('loginerror.html')
+            return render_template('launchinstances.html')
         
-
 @app.route("/logout", methods=['POST', 'GET'])
 def logout():
-    session.pop('user', None)
     return render_template('logout.html')
 
 @app.route("/launchinstances", methods=['POST', 'GET'])
 def launchinstances():
-
-    return render_template('launchinstances.html')
+    form = LaunchInstanceForm()
+    form.customer.choices = [(customers.id,customers.cname)for customers in Customers.query.all()]
+    form.product.choices =[(products.id, products.pname)for products in Products.query] #.filter_by(id='1').all()
+    form.version.choices = [(versions.id, versions.version)for versions in Versions.query]
+    form.component.choices = [(components.id, components.compname)for components in Components.query]
+    return render_template('launchinstances.html', form=form)
 
 @app.route("/userservicelist", methods=['POST', 'GET'])
 def userservicelist():
@@ -65,10 +151,48 @@ def review():
     return render_template('reviewinstances.html')
 
 # This particular route is for testing purposes only 
-@app.route("/testpage", methods=['POST', 'GET'])
+
+@app.route("/testpage/", methods=['POST', 'GET'])
 def testpage():
-    
-    #customer = form.customer.data
-    return render_template('testpage.html')
+    form = LaunchInstanceForm()
+    form.customer.choices = [(customers.id,customers.cname)for customers in Customers.query.all()]
+    form.product.choices = []#[(products.id, products.pname)for products in Products.query] #.filter_by(id='1').all()
+    form.version.choices = []#[(versions.id, versions.version)for versions in Versions.query]
+    form.component.choices = []  #[(components.id, components.compname)for components in Components.query]
+    return render_template('testpage.html', form=form )
+
+@app.route("/testpage/<customerid>", methods=['POST', 'GET'])
+def get_products_per_customer(customerid):
+        products_per_customer = db.session.query(Products.pname, Products.id).join(Versions, Products.id==Versions.product_id).filter(Versions.product_id==customerid).all()#.distinct(Products.id)
+        print(products_per_customer)
+        availableproducts = []
+        for items in products_per_customer:
+            products={}
+            products['id'] = items.id
+            products['product'] = items.pname
+            availableproducts.append(products)
+            print(products)
+        print(availableproducts)
+        return jsonify({"products" : availableproducts})
+
+@app.route("/versions/<productid>", methods=['POST', 'GET'])
+def get_versions_per_product(productid):
+        versions_per_product = db.session.query(Versions.id, Versions.version, Products).join(Versions, Products.id==Versions.product_id).filter(Versions.product_id==productid).all()
+        print(versions_per_product)
+        availableversions = []
+        for items in versions_per_product:
+            versions={}
+            versions['id'] = items.id
+            versions['version'] = items.version
+            availableversions.append(versions)
+            print(versions)
+        print(availableversions)
+        return jsonify({"versions" : availableversions})
+
+#========ALL CODE ENDS HERE============
+
+
+
+#========RUN APP WITH PRE-DEFINED CONFIGS============
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
