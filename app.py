46ba70e52bdecfee40a7b326ab0629b7e7b3da3c
Deeptesh Bhattacharya <deeptesh_b@persistent.com>
2022-02-15 18:56:39 +0530

    all data inserted for each request made

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ db = SQLAlchemy(app)
 class Groups(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     group = db.Column(db.String)
-    users = db.relationship('Users', backref='group')
+    users = db.relationship('Users', backref='groups')
 
 #========TABLE USERS============
 class Users(db.Model):
@@ -53,25 +53,23 @@ class Users(db.Model):
 class Customers(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     cname = db.Column(db.String())
-    versions = db.relationship('Versions', backref='cname')
 
 #========TABLE PRODUCT============
 class Products(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     pname = db.Column(db.String())
-    versions = db.relationship('Versions', backref='pname')
+    versions = db.relationship('Versions', backref='products')
 
 #========TABLE COMPONENT============
 class Components(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     compname = db.Column(db.String())
-    versions = db.relationship('Versions', backref='compname')
+    versions = db.relationship('Versions', backref='components')
 
 #========TABLE VERSION============
 class Versions(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     version = db.Column(db.String)
-    cust_id = db.Column(db.Integer, db.ForeignKey('customers.id')) 
     product_id = db.Column(db.Integer, db.ForeignKey('products.id')) 
     comp_id = db.Column(db.Integer, db.ForeignKey('components.id')) 
 
@@ -83,6 +81,15 @@ class launchrequests(db.Model):
     version = db.Column(db.String)
     component = db.Column(db.String)
     instances = db.Column(db.Integer)
+    createdby = db.Column(db.String)
+    request_id=db.Column(db.Integer, db.ForeignKey('requestsmapping.id'))
+#============================================================================
+class requestsmapping(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    customer = db.Column(db.Integer)
+    createdby=db.Column(db.String)
+    createdate=db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    status=db.Column(db.String)
 #============================================================================
 
 #DB-QUERIES
@@ -96,82 +103,114 @@ class launchrequests(db.Model):
 
 #========FORM-SCHEMA-LAUNCHINSTANCES============
 class LaunchInstanceForm(FlaskForm):
-    customer = SelectField('customer', choices=[])
     product = SelectField('product', choices=[])
     component = SelectField('component', choices=[])
     version = SelectField('version', choices=[])
     instance = StringField('instance')
-
+    customer = SelectField('customer', choices=[])
+    created_by = StringField('createdby')
 
 #==============================================================================
 #Application Routes
 
 @app.route("/", methods=['GET'])
 def index():
-        return render_template('login.html')
+    if 'username' in session:
+        user = session['username']
+        print(user)
+        return render_template('userservicelist.html',user=user)
+    return render_template('login.html')
+
 
 @app.route("/login", methods=['POST', 'GET'])
 def login():
-    return render_template('login.html')
-        
-@app.route("/logout", methods=['POST', 'GET'])
-def logout():
-    return render_template('logout.html')
+    if request.method == 'POST':
+        session['username'] = request.form['userid']
+        return redirect(url_for('index'))
+    return render_template('loginerror.html')
 
-@app.route("/launchinstances", methods=['POST', 'GET'])
-def launchinstances():
-    form = LaunchInstanceForm()
-    form.customer.choices = [(customers.id,customers.cname)for customers in Customers.query.all()]
-    form.product.choices =[(products.id, products.pname)for products in Products.query] #.filter_by(id='1').all()
-    form.version.choices = [(versions.id, versions.version)for versions in Versions.query]
-    form.component.choices = [(components.id, components.compname)for components in Components.query]
-    return render_template('launchinstances.html', form=form)
+@app.route('/logout')
+def logout():
+   # remove the username from the session if it is there
+   session.pop('username', None)
+   return render_template('logout.html')
 
 @app.route("/userservicelist", methods=['POST', 'GET'])
 def userservicelist():
-    return render_template('userservicelist.html')
+    if 'username' in session:
+        user = session['username']
+        return render_template('userservicelist.html',user=user)
+    return render_template('loginerror.html')
+
+@app.route("/launchinstances", methods=['POST', 'GET'])
+def launchinstances():
+    if 'username' in session:
+        user = session['username']
+        
+        
+        form = LaunchInstanceForm()
+        form.customer.choices = [(customers.id,customers.cname)for customers in Customers.query.all()]
+        form.product.choices =[(products.id, products.pname)for products in Products.query] #.filter_by(id='1').all()
+        form.version.choices = [(versions.id, versions.version)for versions in Versions.query]
+        form.component.choices = [(components.id, components.compname)for components in Components.query]
+        return render_template('launchinstances.html', form=form, user=user)
+    return render_template('loginerror.html')
+
+
 
 @app.route("/addtemplate", methods=['POST', 'GET'])
 def addtemplate():
-    return render_template('addtemplate.html')
+    if 'username' in session:
+        user = session['username']
+        return render_template('addtemplate.html',user=user)
+    return render_template('loginerror.html')
 
 @app.route("/cisghome", methods=['POST', 'GET'])
 def cisghome():
-    return render_template('cisgservices.html')
+    if 'username' in session:
+        user = session['username']
+        return render_template('cisgservices.html',user=user)
+    return render_template('loginerror.html')
 
 @app.route("/reviewinstances", methods=['POST', 'GET'])
 def reviewinstances():
-    return render_template('reviewinstances.html')
+    if 'username' in session:
+            user = session['username']
+            return render_template('reviewinstances.html',user=user)
+    return render_template('loginerror.html')                                                   
 
 # This particular route is for testing purposes only 
 
 @app.route("/testpage/", methods=['POST', 'GET'])
 def testpage():
-    form = LaunchInstanceForm()
-    form.customer.choices = [(customers.id,customers.cname)for customers in Customers.query.all()]
-    form.product.choices = ['--select--']#[(products.id, products.pname)for products in Products.query] #.filter_by(id='1').all()
-    form.version.choices = ['--select--']#[(versions.id, versions.version)for versions in Versions.query]
-    form.component.choices = ['--select--']  #[(components.id, components.compname)for components in Components.query]
-    return render_template('testpage.html', form=form )
+    if 'username' in session:
+        user = session['username']
+        form = LaunchInstanceForm()
+        form.customer.choices = [(customers.id,customers.cname)for customers in Customers.query.all()]#[(customers.id,customers.cname)for customers in Customers.query.all()]
+        form.product.choices = [(products.id, products.pname)for products in Products.query.all()] #.filter_by(id='1').all()
+        form.version.choices = ['--select--']#[(versions.id, versions.version)for versions in Versions.query]
+        form.component.choices = ['--select--']#[(components.id, components.compname)for components in Components.query]
+        return render_template('testpage.html', form=form, user=user )
+    return render_template('loginerror.html')
 
-@app.route("/getproducts/<customerid>", methods=['POST', 'GET'])
-def get_products_per_customer(customerid):
-        products_per_customer = db.session.query(Products.pname, Products.id).join(Versions, Products.id==Versions.product_id).distinct(Products.id).all()#
+@app.route("/getproducts", methods=['POST', 'GET'])
+def get_products_per_customer():
+        products_per_customer = db.session.query(Products.id, Products.pname).distinct(Products.id).all()#
         print(products_per_customer)
         availableproducts = []
         for items in products_per_customer:
             products={}
-            products['id'] = items.id
-            products['product'] = items.pname
+            products["id"] = items.id
+            products["product"] = items.pname
             availableproducts.append(products)
-            #print(products)
-        #print(availableproducts)
+            print(products)
+        print(availableproducts)
         return jsonify({"products" : availableproducts})
 
 @app.route("/getversions/<productid>", methods=['POST', 'GET'])
 def get_versions_per_product(productid):
-        versions_per_product = db.session.query(Versions.id, Versions.version, Products).join(Versions, Products.id==Versions.product_id).filter(Versions.product_id==productid).all()
-        #print(versions_per_product)
+        versions_per_product = db.session.query(Versions.id, Versions.version).filter(Versions.product_id==productid).all()
+        print(versions_per_product)
         availableversions = []
         for items in versions_per_product:
             versions={}
@@ -184,8 +223,8 @@ def get_versions_per_product(productid):
 
 @app.route("/getcomponents/<versionid>", methods=['POST', 'GET'])
 def get_components_per_version(versionid):
-        components_per_version = db.session.query(Components.id, Components.compname, Products, Versions).join(Versions, Products.id==Versions.product_id).filter(Versions.id==versionid).all()
-        #print(components_per_version)
+        components_per_version = db.session.query(Components.id, Components.compname,Versions).join(Versions).filter(Versions.id==versionid).filter().all()
+        print(components_per_version)
         availablecomponents = []
         for items in components_per_version:
             component={}
@@ -200,18 +239,22 @@ def get_components_per_version(versionid):
 @app.route("/createformcollection/<strjson>", methods=['POST', 'GET'])
 def createformcollection(strjson):
     if request.method == 'POST':
-        response = json.loads(strjson)
+        response = json.loads(strjson) 
+        print("response recd is")
+        print(response)
+        for items in response:
+            createdby = items['createdby']
+            dqqueryforrequestid = requestsmapping(id=None,customer=items['customer'],createdby=items['createdby'],createdate=None, status=None)
+        db.session.add(dqqueryforrequestid)
+        db.session.commit()
         for i in response:
-            insertdatafordb=launchrequests(id=None,customer=i['customer'],product=i['product'],version=i['version'],component=i['component'],instances=i['instance'])
+            requestid=dqqueryforrequestid.id
+            insertdatafordb=launchrequests(id=None,customer=i['customer'],product=i['product'],version=i['version'],component=i['component'],instances=i['instance'], createdby=i['createdby'],request_id=requestid)
             db.session.add(insertdatafordb)
-            db.session.commit()
-            db.session.close()
-            #print('successful')
+        db.session.commit()
+        db.session.close()
+        #print('successful')
     return ("success")
-
-
-
-    
 
 #========ALL CODE ENDS HERE============
 
